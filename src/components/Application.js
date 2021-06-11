0f29902b6ecdfac20bbb6645bb1cce6790347ed6
Pavneet Kang <pavneet.kang2@gmail.com>
2021-06-11 01:18:48 -0700

    implement interviewers for day in form

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 import "components/Appointment"
 import Appointment from "components/Appointment";
 import axios from 'axios';
-import {getAppointmentsForDay, getInterview} from 'helpers/selectors'
+import {getAppointmentsForDay, getInterview, getInterviewersForDay} from 'helpers/selectors'
 
 /*
 const appointments = [
@@ -93,10 +93,11 @@ export default function Application(props) {
   }, [])
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
+
   let mappedAppointments = dailyAppointments.map((appointment)=>{
     const interview = getInterview(state, appointment.interview);
-    return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview}/>
+    return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview} interviewers={dailyInterviewers}/>
   })
   
   return (
